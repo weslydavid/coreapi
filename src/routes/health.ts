0d955da71474955457879453fb8e60865439aa63
weslydavid <wescamilla@mutualser.org>
2023-10-10 14:01:26 -0500

    Cambio de nombre

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (request: Request, response: Response) => {
-  response.json({ nombre: '´Wesly David' });
+  response.json({ nombre: '´Wesly David Escamilla' });
 });
 
 export default router;
