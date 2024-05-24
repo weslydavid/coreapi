e1b9a891d31b65ae4cd359aff8f6a2ffdf6f7f05
kelvin hernandez cabrera <kelhzkb@hotmail.com>
2024-05-24 15:12:16 +0200

    STUDIO-01: refactor the validate schema method

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,28 @@
 import { Router } from 'express';
 const router = Router();
 import usersController from '../controllers/users/users.controller';
+import validateSchema from '../middlewares/validateSchema';
+import userSchemas from '../controllers/users/users.schema';
 
 // Get all users.
-router.get('/', usersController.getUsers);
+router.get(
+  '/',
+  validateSchema(userSchemas.getAllUsers),
+  usersController.getUsersHandler,
+);
 
 // Register a new user.
-router.post('/register', usersController.register);
+router.post(
+  '/register',
+  validateSchema(userSchemas.register),
+  usersController.registerHandler,
+);
 
 // Get user by id.
-router.get('/:id', usersController.getUserById);
+router.get(
+  '/:id',
+  validateSchema(userSchemas.getUserById),
+  usersController.getUserByIdHandler,
+);
 
 export default router;
